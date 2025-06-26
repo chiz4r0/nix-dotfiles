db60f1295d16272dbc512b1e7c24977fb736bc21
chiz4r0 <yurandarezky@gmail.com>
2025-06-26 12:43:29 +0700

    my fist flake dotfile

--- configuration.nix
+++ configuration.nix
@@ -2,12 +2,13 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.home-manager
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -96,6 +97,19 @@
     packages = with pkgs; [
       tree
     ];
+  };
+
+  ### Home Manager
+  home-manager = {
+    useUserPackages = true;
+    useGlobalPkgs = true;
+    users.tabun = {
+      imports =  [ ./home.nix ]
+      home.username = "tabun";
+      home.homeDirectory = "/home/tabun";
+      home.stateVersion = "24.05";
+      programs.home-manager.enable = true;
+    };
   };
 
   programs.firefox.enable = true;
---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = 
+  { nixpkgs, self, ... } @inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+    lib = nixpkgs.lib;
+  in
+  {
+    nixosConfigurations = {
+      catLinux = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+        specialArgs = {
+          inherit self inputs;
+        };
+      };
+    };
+  };
+}
