7896ec70d468c1e994d876800c32ca17cc7bbd6f
chiz4r0 <yurandarezky@gmail.com>
2025-07-02 17:47:54 +0700

    use latest kernel

--- modules/system.nix
+++ modules/system.nix
@@ -3,6 +3,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   zramSwap.enable = true;
 
   time.timeZone = "Asia/Jakarta";
--- nixos/default.nix
+++ nixos/default.nix
@@ -4,17 +4,16 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./modules/desktop.nix
-      ./modules/flatpak.nix
-      ./modules/font.nix
-      ./modules/hardware.nix
-      ./modules/networking.nix
-      ./modules/package.nix
-      ./modules/print.nix
-      ./modules/system.nix
-      ./modules/tlp.nix
-      ./modules/users.nix
-      ./modules/virtualisation.nix
+      ../modules/desktop.nix
+      ../modules/flatpak.nix
+      ../modules/font.nix
+      ../modules/hardware.nix
+      ../modules/networking.nix
+      ../modules/package.nix
+      ../modules/print.nix
+      ../modules/system.nix
+      ../modules/tlp.nix
+      ../modules/users.nix
+      ../modules/virtualisation.nix
     ];
-
 }
